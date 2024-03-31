c6f50db429018b35b9555f8b8c4dbf9ba1b4ebd3
TopboySaint <topboysaint1@gmail.com>
2024-03-31 10:51:19 +0100

    almost done

---
+++ script.js
@@ -0,0 +1,11 @@
+const calculate = () => {
+    if(manName.value ==="" || womanName.value ===""){
+        document.getElementById('alert').style.display = "block"
+    } else{
+        document.getElementById('alert').style.display = "none"
+        document.getElementById('manName').value = ""
+        document.getElementById('womanName').value = ""
+        let answer = Math.round(Math.random()*100)
+        document.getElementById('show').innerHTML = `You're ${answer}% Compatible`
+    }
+}
