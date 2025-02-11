f95dd055f7e8fb40bf28e547bcf4cba51a248ca7
TopboySaint <topboysaint1@gmail.com>
2025-02-11 01:35:00 +0100

    Refactor layout and styles for improved responsiveness and user experience

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const calculate = () => {
     document.getElementById("alert").style.display = "block";
   } else {
     document.getElementById("alert").style.display = "none";
-    document.getElementById("manName").value = "";
-    document.getElementById("womanName").value = "";
+    // document.getElementById("manName").value = "";
+    // document.getElementById("womanName").value = "";
     let answer = Math.round(Math.random() * 100);
     document.getElementById("show").innerHTML = `You're ${answer}% Compatible`;
   }
